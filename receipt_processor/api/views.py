7614f3a57910d71614dbb87b12cb3ad6d488e776
Will Barker <willbarker34@gmail.com>
2024-07-22 14:23:09 -0400

    Added docstrings for endpoint clarity

--- receipt_processor/api/views.py
+++ receipt_processor/api/views.py
@@ -13,6 +13,18 @@ receipts = {}
 
 @method_decorator(csrf_exempt, name="dispatch")
 class ProcessReceiptView(View):
+    """
+        POST /receipts/process
+        Processes a JSON receipt and returns an ID.
+        
+        Payload: JSON receipt (See example file in original challenge repo)
+        Response: {"id": "receipt_id"}
+        
+        Example Response:
+        {
+            "id": "7fb1377b-b223-49d9-a31a-5a02701dd310"
+        }
+    """
     def post(self, request, *args, **kwargs):
         receipt_data = json.loads(request.body)
         receipt_id = uuid.uuid4()
@@ -22,6 +34,18 @@ class ProcessReceiptView(View):
     
 
 class GetPointsView(View):
+    """
+        GET /receipts/{id}/points
+        Returns the points for a receipt ID.
+        
+        Response: {"points": number_of_points}
+        Error: {"error": "Receipt not found"}
+        
+        Example Response:
+        {
+            "points": 32
+        }
+    """
     def get(self, request, receipt_id, *args, **kwargs):
         points = receipts.get(str(receipt_id))
         if points is not None:
