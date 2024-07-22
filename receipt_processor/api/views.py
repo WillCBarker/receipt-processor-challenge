969c5660cb0b8bf121dcc9422a383e601d459ee6
Will Barker <willbarker34@gmail.com>
2024-07-22 14:18:33 -0400

    Process Receipts endpoint with POST method

--- receipt_processor/api/views.py
+++ receipt_processor/api/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+import json
+import uuid
+from django.http import JsonResponse
+from django.utils.decorators import method_decorator
+from django.views import View
+from django.views.decorators.csrf import csrf_exempt
+from .util import PointsCalculator
 
-# Create your views here.
+
+# temp storage
+receipts = {}
+
+
+@method_decorator(csrf_exempt, name="dispatch")
+class ProcessReceiptView(View):
+    def post(self, request, *args, **kwargs):
+        receipt_data = json.loads(request.body)
+        receipt_id = uuid.uuid4()
+        points = PointsCalculator.calculate_points(receipt_data)
+        receipts[str(receipt_id)] = points
+        return JsonResponse({"id": str(receipt_id)})
